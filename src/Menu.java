81bab986ac9c4b73b740d7a2e29dbc1138bffca6
ShowKin <3064219762@qq.com>
2026-03-26 21:02:42 +0800

    aaa

--- src/Menu.java
+++ src/Menu.java
@@ -1,8 +1,10 @@
+import java.io.FileWriter;
 import java.time.Duration;
 import java.time.LocalTime;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
-
+import java.io.File;
+import java.io.IOException;
 class person {
     AtomicInteger p = new AtomicInteger(0);
     LocalTime  start;
@@ -13,6 +15,8 @@ public class Menu {
     private static final Object scannerLock = new Object();
 
     public static void main(String[] args) {
+        File record = new File("record.txt");
+        startPrepare(record);
         person p=new person();
         Scanner sc = new Scanner(System.in);
            //  菜单循环开始
@@ -59,7 +63,30 @@ public class Menu {
 
     }
     public static void TodayAllStadyTime() {
+        File record = new File("record.txt");
 
+        //读取上次记录的时间，判断是否是今天
+        //如果是今天，则计算时间
+        //如果不是今天，则重新开始记录
+        //记录时间
+        //保存时间
+    }
+    public static void startPrepare(File record) {
+        try{
+            if(record.exists()){
+                return;
+            }else {
+
+                record.createNewFile();
+                FileWriter fw = new FileWriter(record,true);
+                fw.write(LocalTime.now().toString()+"\n");
+                fw.write(0+"\n");
+                fw.write(0+"\n");
+                fw.close();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
 }
